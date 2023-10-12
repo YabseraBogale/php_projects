06a710e201e66815261448169eaabb3be95f4e92
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-12 16:25:34 +0300

    reading life

---
+++ github_tool/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tool</title>
+</head>
+<body>
+    <h1>
+        Git hub Tool
+        
+    </h1>
+    <p>commit you want ?</p>
+    <form action="index.php" method="post">
+       <p>commit I give <input type="number" name="number" id="number"> <input type="submit" value="Enter"></p>
+    </form>
+</body>
+</html>
