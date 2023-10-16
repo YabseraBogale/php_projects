d73bb1e59c5459ed87c367005516a2b71696d73f
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-16 17:37:18 +0300

    ok

--- github_tool/git_actions/main.php
+++ github_tool/git_actions/main.php
@@ -3,16 +3,10 @@ class MyDB extends SQLite3
 {
     function __construct()
     {
-        $this->open(__DIR__.'/database/word.db');
+        $this->open(__DIR__.'/database/git_Actions.db');
     }
 }
 
-$db = new MyDB();
-$result = $db->query('SELECT Pword FROM Python');
 
-while($row =$result->fetchArray()){
-    var_dump($row['Pword']);
-    echo '<br>';
-}
 
 ?>
