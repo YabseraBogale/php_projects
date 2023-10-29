0e1c4a7471023e886856b68dca0aa53529b1a0c7
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-29 14:08:49 +0300

    mummy

--- php_chess_board/index.php
+++ php_chess_board/index.php
@@ -2,4 +2,17 @@
 	<head>
 		<link rel="stylesheet" href="style.css">
 		</head>
+		<title>Chess Board</title>
+		<body>
+		<?php
+			for($i=0;$i<8;$i++){
+				for($j=0;$j<8;$j++){
+					echo "";
+				
+				}
+				echo "<br>";
+			}
+		
+		?>
+		</body>
 	</html>
