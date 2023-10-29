b080357e77a86a3eb444ab08833d301f427acfb3
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-29 14:12:55 +0300

    pp

--- php_chess_board/index.php
+++ php_chess_board/index.php
@@ -7,8 +7,11 @@
 		<?php
 			for($i=0;$i<8;$i++){
 				for($j=0;$j<8;$j++){
-					echo "";
-				
+					$c="white";
+					if(($i+$j)%2==0){
+						$c="red";
+					}
+					echo "<div style='color: $c'></div>"
 				}
 				echo "<br>";
 			}
