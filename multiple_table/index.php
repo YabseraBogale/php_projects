85d4dad990f34afdf6943b5b92a80f77a562bb44
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-29 16:55:45 +0300

    linux

--- multiple_table/index.php
+++ multiple_table/index.php
@@ -5,29 +5,37 @@
 	<body>
 		<?php
 		for($i =1;$i<12;$i++){
+			echo "<div class='main'>";
 			for($j =1;$j<12;$j++){
 				$mu=$j*$i;
 				$isPrime=true;
 				for($k=2;$k<(Integer)sqrt($mu);$k++){
-					if($mu==1){
-						$isPrime=false;
-						break;
+					switch($mu){
+						case $mu==1:
+							isPrime=false;
+							break;
+						case $mu==2:
+							break;
+						case $mu==3:
+							break;
+						case $mu==5:
+							break;
+						case $mu==7:
+							break;
+						case $mu%$k==0:
+							isPrime=false;
+							break;	
 					}
-					else if($mu%$k==0){
-						$isPrime=false;
-						break;
-					}
-					
 				}
 				if($isPrime==true){
-					echo '<div class="prime">$mu</div>';
+					echo "<div class='prime'>$mu</div>";
 				}
 				else{
-					echo '<div class="notPrime">$mu</div>';
+					echo "<div class='notPrime'>$mu</div>";
 				}
 				
 			}
-			echo "<br>";
+			echo "</div>";
 		}
 		?>
 	
