06ed7f6ac88e0e947a7d8d7dcd5d8b97c2552b48
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-06 19:43:41 +0300

    php ok

--- fileupload/index.php
+++ fileupload/index.php
@@ -1,1 +1,11 @@
-<>
+<html>
+	<head>
+		<title>File upload</title>
+		</head>
+	<body>
+		<form method='post' action='./index.php'>
+			<h1>File Input</h1>
+				<p>Enter File <input type="file"></p>
+			</form>
+		</body>
+	</html>
