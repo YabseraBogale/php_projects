f09468baf9b5f33739c3cebd8ec0c710b11a3fea
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-07 14:05:49 +0300

    ok

---
+++ class_ass/calculator.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="./style.css">
+		<title>simple calcultor</title>
+		</head>
+		<body>
+			<form method="post" action ="./caculator.php">
+				
+			
+			</form>
+		</body>
+	</html>
