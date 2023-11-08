7718624fd7dee3d3406b80d821660f903850e122
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-08 14:34:40 +0300

    falied

--- class_ass/image.php
+++ class_ass/image.php
@@ -1,10 +1,40 @@
 
 <html>
 	<head>
+		<link rel="stylesheet" href="./image.css">
 		</head>
 		<body>
+			<?php 
+				$page=['home'=>'home.php',
+						'about'=>'about.php',
+						'login'=>'login.php',
+						'signup'=>'sign.php'];
+			?>
+			<nav>
+				<ul>
+					<?php
+						foreach($page as $key=>$value){
+							?>
+							<li><a href="./<?php
+								$value
+							?>"><?php
+							echo $key;
+							?>
+							<?php
+						}
+					?>
+					</ul>
+				</nav>
+			
+			
+			
 			<div class="main-img">
-				<img href="./image/1.jpeg">
+				<?php for($i=1;$i<=5;$i++){
+					?><img src="./image/<?php
+					echo "$i.jpeg";
+				?>"><?php
+					echo "<br>";}
+				?>
 				</div>
 			</body>
 	</html>
