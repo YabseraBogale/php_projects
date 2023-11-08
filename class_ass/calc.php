cbc98c41c29dada1a1d0fde0c19da51b8c5d254e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-08 14:06:02 +0300

    falied

--- class_ass/calc.php
+++ class_ass/calc.php
@@ -1,17 +1,50 @@
-<?php
-	$numbers=[1,2,3,4,5,6,7,8,9,0];
-	$operation=['+','-','*','/'];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamic Div</title>
+    <style>
+        body{
+            width:100%;
+            height:100vh;
+        }
+        h1{
+            text-align:center;
+        }
+        .container{
+            width:100%;
+            min-height:250px;
+            background:gray;
+            display:grid;
+            grid-template-columns: auto auto auto auto;
+            
+        }
 
-<html>
-	<body>
-		<form method="post">
-			<?php
-				for($i=0;$i<3;$i++){
-					echo "<button>$numbers[$i]</button>";
-				}
-				
-			?>
-		</form>
-		</body>
-	</html>
+        .container .item{
+            width:220px;
+            height:220px;
+            margin:1rem;
+            background: #fff;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Dynamic Page using For loop</h1>
+    <div class="container">
+        
+        <?php
+        $items = [1,2,3,4,5,6,7,8,9];
+        foreach($items as $item){
+            ?>
+            <div class="item">
+                <?php echo $item; ?>
+            </div>
+
+        <?php
+        }
+        ?>
+    </div>
+</body>
+</html>
