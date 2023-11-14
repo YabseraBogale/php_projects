4095ad25bc2b7a277e99002219df7496f0ae0e2f
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-14 13:31:01 +0300

    ok

---
+++ reading-json/perefomance.php
@@ -0,0 +1,39 @@
+
+// this worked fine but the time-space complexity kills the pc with its n^2
+<html>
+	<head>
+		<link rel="stylesheet" href="./style.css">
+		<title>Reading Json</title>
+		</head>
+		<body>
+			<?php
+				$json=file_get_contents('smu_cs_club_messages.json');
+				$json_data=json_decode($json,true);
+				foreach($json_data as $key=>$value){
+					?>
+						
+						<br>
+						<div class="array"><?php 
+							foreach($value as $_key=>$_value){
+								?>
+	
+								<br>
+								<p><?php 
+									if($_value["message"]!=""){
+										?>
+										<h5><?php echo $_key;?></h5>
+										<?php
+										echo $_value["message"];
+									}
+								
+								?></p>
+								<br>
+								<?php
+							}
+						?></div>	
+						<br>
+					<?php		
+				}
+			?>
+		</body>
+	</html>
