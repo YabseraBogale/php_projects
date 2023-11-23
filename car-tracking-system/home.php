b21564894fc8e78c2dbd4715496b02c74201ec9a
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-23 19:31:16 +0300

    ok

--- car-tracking-system/home.php
+++ car-tracking-system/home.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
     <title>Home</title>
 </head>
 <body>
@@ -15,9 +16,11 @@
                     ['regsister'=>'regsister.php'],
                 ];
                 foreach($pages as $page){
-                    ?>
-                    <li><a href="<?php echo $page['home']; ?>"><?php echo ?></a></li>
-                    <?php
+                    foreach($page as $key=>$value){
+                        ?>
+                        <li><a href="<?php echo $value?>"><?php echo $key;?></a></li>
+                        <?php
+                    }
                 }
             ?>
         </ul>
