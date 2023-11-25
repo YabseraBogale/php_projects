3458d10744399e63f55aa13dbbbd28ee065ca27b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-25 16:54:39 +0300

    ok

--- car-tracking-system/database.sql
+++ car-tracking-system/database.sql
@@ -9,27 +9,28 @@ create table User(
 
 create table location(
 
-    DriverID
+   
 );
 
 create table Driver(
 
     FirstName varchar(20) not null,
     LastName varchar(20) not null,
+    -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
     UserId int not null,
     PlateNumberId varchar(10) not null 
 
 );
 
 create table PlateNumber(
-
+    -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
     UserId int not null,
     PlateNumberId varchar(10) not null
 
 );
 
 create table PrivateOwner(
-    
+     -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
     Id int not null,
     TypeofCar text not null,
     -- thinking of limiting it
@@ -39,7 +40,7 @@ create table PrivateOwner(
 );
 
 create table Company(
-
+     -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
     Id int not null,
     TypeofCar text not null,
     -- thinking of limiting it
