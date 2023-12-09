4dca0fe285b4b1f49d7f574bd3207505f4c7829e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-09 18:18:50 +0300

    ok

--- car-tracking-system/database.sql
+++ car-tracking-system/database.sql
@@ -1,18 +1,55 @@
-create table Car(
-    PlateNumber int not null,
-    Name varchar(30) not null,
-    DriverId int not null,
-    CurrentLocation json not null,
-    Destination json not null,
-    Departure json not null,
-    DriverId int not null
-)
-create table Driver(
+create table User(
+    -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
     Id int not null,
-    NumberOfCar int null,
-    FirstName varchar(30) not null,
-    LastName varchar(30) not null,
-    PhoneNumber int(12) not null,
-    Gender varchar(1) not null,
-    BloodType varchar(1) not null
-)
+    TypeOfUser varchar(10) not null,
+    NumberOfCar int not null
+
+);
+
+
+create table location(
+
+     -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
+    DriverId int not null,
+    latitude float not null,
+    longitude float not null,
+    PlateNumberId int not null,
+   
+);
+
+create table Driver(
+
+    FirstName varchar(20) not null,
+    LastName varchar(20) not null,
+    -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
+    UserId int not null,
+    PlateNumberId varchar(10) not null 
+
+);
+
+create table PlateNumber(
+    -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
+    UserId int not null,
+    PlateNumberId varchar(10) not null
+
+);
+
+create table PrivateOwner(
+     -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
+    Id int not null,
+    TypeofCar text not null,
+    -- thinking of limiting it
+    NumberOfCar int not null,
+    DriverId int not null
+
+);
+
+create table Company(
+     -- thinking of making it varchar with 15 charaters with 0-9a-zA-Z
+    Id int not null,
+    TypeofCar text not null,
+    -- thinking of limiting it
+    NumberOfCar int not null,
+    DriverId int not null,
+
+);
