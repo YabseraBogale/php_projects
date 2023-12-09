f79194f856b26b034f43eba8237648cb77605db1
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-09 18:10:28 +0300

    ok

---
+++ car-tracking-system/database.sql
@@ -0,0 +1,9 @@
+create table Car(
+    PlateNumber int not null,
+    Name varchar(30) not null,
+    DriverId int not null,
+    CurrentLocation json not null,
+    Destination json not null,
+    Departure json not null,
+    DriverId int not null,
+)
