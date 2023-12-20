067b583bde4f7031fcd92a89d54869e594f11764
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-20 18:41:15 +0300

    ok

--- car/Model/header.php
+++ car/Model/header.php
@@ -1,14 +1,7 @@
 <?php
     class HeaderModel{
         function Nav(){
-           ?>
-           <nav>
-                <a class="header" href="">Home</a>
-                <a class="header" href="">Home</a>
-                <a class="header" href="">Home</a>
-                <a class="header" href="">Home</a>
-           </nav>
-           <?php
+           return "Hello";
         }
     }
 
