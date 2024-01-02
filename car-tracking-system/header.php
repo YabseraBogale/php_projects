d6f3afd0fb2e7db1fd0cbe6fd3af932a6abcb327
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-02 17:11:27 +0300

    ok

--- car-tracking-system/header.php
+++ car-tracking-system/header.php
@@ -2,10 +2,23 @@
 class Head{
 
     public function Navigation(){
-
+        $nav="
+                <style>
+                </style>
+                <nav class='head'>
+                    <a>link1</a>
+                    <a></a>
+                    <a></a>
+                    <a></a>
+                    
+                </nav>
+        
+        ";
+        echo $nav;
     }
 
 }
-
+$nav=new Head();
+$nav.Navigation();
 
 ?>
